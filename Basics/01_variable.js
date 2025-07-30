80d70ab46f0e7c2c1130f9843e377201d7358d45
AllysiaDattaLearn <datta.allysia07@gmail.com>
2025-07-30 04:38:34 +0000

    dataTypes

--- Basics/01_variable.js
+++ Basics/01_variable.js
@@ -1,6 +1,6 @@
 const accountID = 100
 let accountEmail = "test@gmail"
-var accountPassword = "12345"
+var accountPassword = "12345" //var is not used anymore due to issue with block scope and functional scope
 accountCity = "Kolkata" //not a good practice but can be usedin situations
 let accountState;
 
@@ -11,5 +11,13 @@ accountPassword = "111111"
 accountCity = "Mumbai"
 
 console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
+/* Output:
+─────────┬───────────────┐
+│ (index) │ Values        │
+├─────────┼───────────────┤
+│ 0       │ 100           │
+│ 1       │ 'check@gmail' │
+│ 2       │ '111111'      │
+│ 3       │ 'Mumbai'      │
+│ 4       │ undefined     │  */
 
-//var is not used anymore due to issue with block scope and functional scope
---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+let myName = "Allysia"
+let age = 30
+let type;
+
+//Premitive data types
+//number => up to 2 to power 53
+//bigint => huge number - not much used
+//string => ""
+//boolean => true/false
+//null => type is object
+//undefined => type is undefined
+//symbol => unique
+
+//you can print type of anything using below
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+console.log(typeof myName) //string
+console.log(typeof age) //number
+console.log(typeof type) //undefined
+
