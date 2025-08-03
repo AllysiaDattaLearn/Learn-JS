0db90077da5a6b273f093f04a1d96cbf48d45665
AllysiaDattaLearn <datta.allysia07@gmail.com>
2025-08-03 04:42:54 +0000

    push 08/02

---
+++ Basics/05_strings.js
@@ -0,0 +1,6 @@
+const gameName = new String('allysia')
+console.log(gameName[0]) //a this is not an Array, its key value pair
+console.log(gameName.toUpperCase()) //ALLYSIA
+console.log(gameName.length) //7
+console.log(gameName.charAt(2)) //l
+console.log(gameName.indexOf('l')) //1
