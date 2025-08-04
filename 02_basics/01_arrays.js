bb8482c2bb11da6e209b17a414d1cf5ce7ad325d
AllysiaDattaLearn <datta.allysia07@gmail.com>
2025-08-04 06:20:43 +0000

    array more

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -33,3 +33,42 @@ const myArrSplice = myArr2.splice(1, 3)
 console.log("splice array ", myArrSplice) // [ 1, 2, 3 ] includes edge value
 console.log("original array after slice", myArr2) //[ 0, 4, 5, 6 ] original array is updated
 
+const marvel_heros = ["Thor", "Ironman", "Spiderman"]
+const dc_heros = ["Superman", "Flash", "Batman"]
+
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros) //[ 'Thor', 'Ironman', 'Spiderman', 'Superman', 'Flash', 'Batman' ]
+//concat both arrays and create a new array
+
+marvel_heros.push(dc_heros)
+console.log(marvel_heros) //[ 'Thor', 'Ironman', 'Spiderman', [ 'Superman', 'Flash', 'Batman' ] ]
+//dc_heros array is added as last index
+console.log(marvel_heros[3]) //[ 'Superman', 'Flash', 'Batman' ]
+
+const marvel_heros1 = ["Thor", "Ironman", "Spiderman"]
+const dc_heros1 = ["Superman", "Flash", "Batman"]
+const indian_heros1 = ["Shaktiman", "nagaraj"]
+
+const all_heros = [...marvel_heros1, ...dc_heros1, ...indian_heros1]
+console.log(all_heros) //["Thor", "Ironman", "Spiderman", "Superman", "Flash", "Batman", "Shaktiman", "nagaraj"]
+//using spread operator, spread all values and adds in an array, mostly used
+
+const nestedArray = [1, 2, [3, 4, 5], 6, [5, 6, [7, 8]]]
+const realArray = nestedArray.flat(Infinity)
+console.log(realArray) //[1, 2, 3, 4, 5, 6, 5, 6, 7, 8]
+
+console.log(Array.isArray("Allysia")) //false do you have this array?
+console.log(Array.from("Allysia")) //['A', 'l', 'l', 'y', 's', 'i', 'a'] makes array from the input
+console.log(Array.from({name: "Allysia"})) //[] return empty array. you have to specify whether to make array with keys or values....discuss later
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3)) //[ 100, 200, 300 ]
+
+
+
+
+
+
+
